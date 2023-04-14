41d83fc7104c51c5decbdcfadc90d81f0f04b060
Azam <Azam@AZAM-PC>
2023-04-14 22:30:02 +0530

    committed

--- MyMvcWebApp10pm/Controllers/AboutUsController.cs
+++ MyMvcWebApp10pm/Controllers/AboutUsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyMvcWebApp10pm.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -37,9 +38,65 @@ namespace MyMvcWebApp10pm.Controllers
         }
 
 
+        public ActionResult SendData()
+        {
+            ViewBag.info = "Hello World";
+            ViewBag.info1 = "Hello World 1";
+            return View();
+        }
+
+
+        public ActionResult SendData2()
+        {
+            EmployeeModel obj = new EmployeeModel();
+            obj.EmpId = 1;
+            obj.EmpName = "Shiva";
+            obj.EmpSalary = 10000;
+
+            ViewBag.Emp = obj;
+
+           
+            return View();
+        }
+
+        public ActionResult SendData3()
+        {
+            EmployeeModel obj = new EmployeeModel();
+            obj.EmpId = 1;
+            obj.EmpName = "Shiva";
+            obj.EmpSalary = 10000;
+
+
+            EmployeeModel obj1 = new EmployeeModel();
+            obj1.EmpId = 2;
+            obj1.EmpName = "chiranjeevi";
+            obj1.EmpSalary = 20000;
+
+
+            EmployeeModel obj2 = new EmployeeModel();
+            obj2.EmpId = 3;
+            obj2.EmpName = "Sabita";
+            obj2.EmpSalary = 30000;
+
+
+            List<EmployeeModel> listObj = new List<EmployeeModel>();
+            listObj.Add(obj);
+            listObj.Add(obj1);
+            listObj.Add(obj2);
+
+            ViewBag.listEmp = listObj;
+
+
+            return View();
+        }
+
+
+
+
 
     }
 }
 
 //calling index method to get Contactus View page  ----Done
 //calling the index method to get dashboard view page of default controller-----
+//how to pass data from controller to view page single string , object,list of Object
---
+++ MyMvcWebApp10pm/Models/EmployeeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyMvcWebApp10pm.Models
+{
+    public class EmployeeModel
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public int EmpSalary { get; set; }
+    }
+
+
+}
